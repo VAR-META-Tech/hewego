1364dd170c1a69b5cde90e7de5adb97477a5794f
namphan22 <phanvanhoainam22@gmail.com>
2024-08-14 08:48:35 +0700

    feat: add lender transaction repo

--- apps/server/src/modules/bond/bond.module.ts
+++ apps/server/src/modules/bond/bond.module.ts
@@ -4,10 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Bond } from 'database/entities/Bond.entity';
 import { BondController } from './bond.controller';
 import { PortfolioController } from './portfolio.controller';
-import { BondCheckout } from 'database/entities';
+import { BondCheckout, LenderTransaction } from 'database/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bond, BondCheckout])],
+  imports: [TypeOrmModule.forFeature([Bond, BondCheckout, LenderTransaction])],
   providers: [BondService],
   controllers: [BondController, PortfolioController],
   exports: [BondService],
