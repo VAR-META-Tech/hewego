d94f25dea066818fd4cac677121957d908977a72
namphan22 <phanvanhoainam22@gmail.com>
2024-08-15 09:41:43 +0700

    fix: get holding bond

--- apps/server/src/modules/bond/bond.service.ts
+++ apps/server/src/modules/bond/bond.service.ts
@@ -387,7 +387,7 @@ export class BondService {
         .addSelect(
           `
         CASE 
-          WHEN bonds.repaid_at IS NOT NULL OR bond_checkout.claimed_at IS NOT NULL OR bonds.liquidated_at IS NOT NULL THEN '${HoldingBondStatus.ENABLE_CLAIM}'
+          WHEN bonds.repaid_at IS NOT NULL AND bond_checkout.claimed_at IS NOT NULL THEN '${HoldingBondStatus.ENABLE_CLAIM}'
           ELSE '${HoldingBondStatus.DISABLE_CLAIM}'
         END as "status"`,
         )
