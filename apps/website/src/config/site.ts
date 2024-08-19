c242e963420fa848aebb3446b244e34159ffc064
Tu <tuphan694@gmail.com>
2024-08-19 09:56:45 +0700

    feat: seo

--- apps/website/src/config/site.ts
+++ apps/website/src/config/site.ts
@@ -6,5 +6,5 @@ export const siteConfig = {
   name: 'Hedera Hackathon',
   description: 'Hedera Hackathon',
   url: env.APP_URL,
-  ogImage: 'http://localhost:3000/opengraph-image.jpg',
+  ogImage: `${env.APP_URL}/opengraph-image.png`,
 };
